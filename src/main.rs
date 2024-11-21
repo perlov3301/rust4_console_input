e60e6d0e5251f3f37b640673a6513dca15f9608c
alexander perlov <perlov3301@gmal.com>
2024-11-21 13:23:50 +0200

    String Object with defined value

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,15 @@ fn main() {
     println!("the prelude is the list of things that rust automatically imports");
     println!("for example:fn,println. Input_output we use(import) from std::io");
     println!("std is standard package; io is module. :: etc. ");
-    let mut xinput = String::new(); // new Object
+    let mut xinput_obj = String::new(); // new Object
     println!("enter something");
     // creating mutable refenence(&) and catching errors
-    io:: stdin().read_line(&mut xinput)
+    io:: stdin().read_line(&mut xinput_obj)
       .expect("failed to read line");
-    println!("just now you have entered: {}", xinput);
+    println!("just now you have entered: {}", xinput_obj);
+
+    let mut xinputObj = String::from("something");
+    println!("before input line");
+    io:: stdin().read_line(&mut xinput_obj).
+       expect("failed to read line");
 }
